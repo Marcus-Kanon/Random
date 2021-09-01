df5d7cce829f9633581178e6b30ba783d3d99f9a
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-09-01 19:15:15 +0200

    Created new project "PageDownloader"

---
+++ PageDownloader/PageDownloader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PageDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adress = System.Console.ReadLine();
+            System.Net.WebClient wc = new System.Net.WebClient();
+            String pageData = wc.DownloadString(adress);
+
+            System.Console.WriteLine(pageData);
+        }
+    }
+}
